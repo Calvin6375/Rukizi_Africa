3d7f672cd51d2820d33ea21ef71325982a7acdae
Rumba <rumba@SULTAN.sultan>
2025-01-02 17:58:25 +0300

    vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.apk'], // Add this line to include .apk files as assets
   build: {
     rollupOptions: {
       input: {
